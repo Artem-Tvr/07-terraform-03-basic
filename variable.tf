7c276792692013abfbbf2be24c3ca281792caeff
Artem_Budin <artyombudin@ya.ru>
2022-07-12 23:51:58 +0300

    112-07

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "aws_access_key" {
-  default = "AKIAZKEAPWXNLZQMBRVO"
+  default = "***************************"
 }
 variable "aws_secret_key" {
-  default = "/8e/dyPE+YfCIuYXJyO6lWkZW2s9YeLVvmruh+es"
+  default = "***************************"
 }
 variable "region" {
     default = "eu-central-1"
